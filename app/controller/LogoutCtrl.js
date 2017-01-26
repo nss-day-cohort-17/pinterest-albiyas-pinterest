f3ba4cf6e9c53458bc01e22919e400299dfb911d
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-26 09:58:38 -0600

    fixed login modal code

--- app/controller/LogoutCtrl.js
+++ app/controller/LogoutCtrl.js
@@ -2,6 +2,6 @@ pinterestApp.controller ("LogoutCtrl", function ($location,$scope,MainFactory){
   $scope.logout = () =>
   MainFactory.logout()
   $location.path('/login')
-  alert("You are no longer signed in")
+  // alert("You are no longer signed in")
   // $scope.$apply()
 })
